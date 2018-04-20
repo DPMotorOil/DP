2e2c880ad6944a6ecad99049d7e0e20bd2695d26
DPMotorOil <37955001+DPMotorOil@users.noreply.github.com>
2018-04-20 15:39:36 +0800

    2018/04/20
    2018/04/20

--- js/index.js
+++ js/index.js
@@ -2,203 +2,229 @@ cards = [
   {
     "id": "product",
     "title": "即將上市",
-    "label": "pro01_label",
+    "intro_title": "敬請期待",
     "target": "#pro01",
     "target_id": "pro01",
-    "img": "./img/product/01.jpg"
+    "img": "./img/product/01.jpg",
+    "intro_img":"./img/product/01.jpg"
   },
   {
     "id": "product",
     "title": "即將上市",
-    "label": "pro02_label",
+    "intro_title": "敬請期待",
     "target": "#pro02",
     "target_id": "pro02",
-    "img": "./img/product/01.jpg"
+    "img": "./img/product/01.jpg",
+    "intro_img":"./img/product/01.jpg"
   },
   {
     "id": "product",
     "title": "即將上市",
-    "label": "pro03_label",
+    "intro_title": "敬請期待",
     "target": "#pro03",
     "target_id": "pro03",
-    "img": "./img/product/01.jpg"
+    "img": "./img/product/01.jpg",
+    "intro_img":"./img/product/01.jpg"
   },
   {
     "id": "product",
     "title": "即將上市",
-    "label": "pro04_label",
+    "intro_title": "敬請期待",
     "target": "#pro04",
     "target_id": "pro04",
-    "img": "./img/product/01.jpg"
+    "img": "./img/product/01.jpg",
+    "intro_img":"./img/product/01.jpg"
   },
   {
     "id": "product",
     "title": "即將上市",
-    "label": "pro05_label",
+    "intro_title": "敬請期待",
     "target": "#pro05",
     "target_id": "pro05",
-    "img": "./img/product/01.jpg"
+    "img": "./img/product/01.jpg",
+    "intro_img":"./img/product/01.jpg"
   },
   {
     "id": "product",
     "title": "即將上市",
-    "label": "pro06_label",
+    "intro_title": "敬請期待",
     "target": "#pro06",
     "target_id": "pro06",
-    "img": "./img/product/01.jpg"
+    "img": "./img/product/01.jpg",
+    "intro_img":"./img/product/01.jpg"
   },
   {
     "id": "product",
     "title": "即將上市",
-    "label": "pro07_label",
+    "intro_title": "敬請期待",
     "target": "#pro07",
     "target_id": "pro07",
-    "img": "./img/product/01.jpg"
+    "img": "./img/product/01.jpg",
+    "intro_img":"./img/product/01.jpg"
   },
   {
     "id": "product",
     "title": "即將上市",
-    "label": "pro08_label",
+    "intro_title": "敬請期待",
     "target": "#pro08",
     "target_id": "pro08",
-    "img": "./img/product/01.jpg"
+    "img": "./img/product/01.jpg",
+    "intro_img":"./img/product/01.jpg"
   },
 
   {
     "id": "experiment",
     "title": "冷啟動模擬器",
-    "label": "exp01_label",
+    "intro_title": "CCS : 冷啟動模擬器 \nASTM : D5293-10",
     "target": "#exp01",
     "target_id": "exp01",
-    "img": "./img/experiment/01.jpg"
+    "img": "./img/experiment/01-s.jpg",
+    "intro_img":"./img/experiment/01.jpg"
   },
   {
     "id": "experiment",
     "title": "高溫高剪切測定儀",
-    "label": "exp02_label",
+    "intro_title": "HTHS : 高溫高剪切測定儀\nASTM : ASTM D-5481-10",
     "target": "#exp02",
     "target_id": "exp02",
-    "img": "./img/experiment/02.jpg"
+    "img": "./img/experiment/02-s.jpg",
+    "intro_img":"./img/experiment/02.jpg"
   },
   {
     "id": "experiment",
     "title": "消泡測試儀",
-    "label": "exp03_label",
+    "intro_title": "ASTM:D892",
     "target": "#exp03",
     "target_id": "exp03",
-    "img": "./img/experiment/03.jpg"
+    "img": "./img/experiment/03-s.jpg",
+    "intro_img":"./img/experiment/03.jpg"
   },
   {
     "id": "experiment",
-    "title": "低溫流動測定儀",
-    "label": "exp04_label",
+    "title": "PPD低溫流動測定儀",
+    "intro_title": "ASTM : D-97",
     "target": "#exp04",
     "target_id": "exp04",
-    "img": "./img/experiment/04.jpg"
+    "img": "./img/experiment/04-s.jpg",
+    "intro_img":"./img/experiment/04.jpg"
   },
   {
     "id": "experiment",
     "title": "MINI-ROTARY VISCOMETER",
-    "label": "exp05_label",
+    "intro_title": "ASTM : D4684-08",
     "target": "#exp05",
     "target_id": "exp05",
-    "img": "./img/experiment/05.jpg"
+    "img": "./img/experiment/05-s.jpg",
+    "intro_img":"./img/experiment/05.jpg"
   },
   {
     "id": "experiment",
     "title": "自動黏度測定儀",
-    "label": "exp06_label",
+    "intro_title": "ASTM : D-445 VI\nASTM : D-2270",
     "target": "#exp06",
     "target_id": "exp06",
-    "img": "./img/experiment/06.jpg"
+    "img": "./img/experiment/06-s.jpg",
+    "intro_img":"./img/experiment/06.jpg"
   },
   {
     "id": "experiment",
     "title": "閉杯式閃火點測定儀",
-    "label": "exp07_label",
+    "intro_title": "ASTM : D-93",
     "target": "#exp07",
     "target_id": "exp07",
-    "img": "./img/experiment/07.jpg"
+    "img": "./img/experiment/07-s.jpg",
+    "intro_img":"./img/experiment/07.jpg"
   },
   {
     "id": "experiment",
     "title": "開杯式閃火點測定儀",
-    "label": "exp08_label",
+    "intro_title": "ASTM : D-92",
     "target": "#exp08",
     "target_id": "exp08",
-    "img": "./img/experiment/08.jpg"
+    "img": "./img/experiment/08-s.jpg",
+    "intro_img":"./img/experiment/08.jpg"
   },
   {
     "id": "experiment",
     "title": "TAN、TBN 酸價、鹼價測",
-    "label": "exp09_label",
+    "intro_title": "TBN ASTM : D-2896\nTAN ASTM : D-664",
     "target": "#exp09",
     "target_id": "exp09",
-    "img": "./img/experiment/09.jpg"
+    "img": "./img/experiment/09-s.jpg",
+    "intro_img":"./img/experiment/09.jpg"
   },
   {
     "id": "experiment",
     "title": "銅板腐蝕測試儀",
+    "intro_title": "ASTM : D-130",
     "label": "exp10_label",
     "target": "#exp10",
     "target_id": "exp10",
-    "img": "./img/experiment/10.jpg"
+    "img": "./img/experiment/10-s.jpg",
+    "intro_img":"./img/experiment/10.jpg"
   },
   {
     "id": "experiment",
     "title": "色相分析儀",
-    "label": "exp11_label",
+    "intro_title": "ASTM : D-1500",
     "target": "#exp11",
     "target_id": "exp11",
-    "img": "./img/experiment/11.jpg"
+    "img": "./img/experiment/11-s.jpg",
+    "intro_img":"./img/experiment/11.jpg"
   },
   {
     "id": "experiment",
     "title": "831KF Coulometer 庫倫式水分測定",
-    "label": "exp12_label",
+    "intro_title": "ASTM : D-492",
     "target": "#exp12",
     "target_id": "exp12",
-    "img": "./img/experiment/12.jpg"
+    "img": "./img/experiment/12-s.jpg",
+    "intro_img":"./img/experiment/12.jpg"
   },
   {
     "id": "experiment",
     "title": "乳化測定儀",
-    "label": "exp13_label",
+    "intro_title": "ASTM : D-140",
     "target": "#exp13",
     "target_id": "exp13",
-    "img": "./img/experiment/13.jpg"
+    "img": "./img/experiment/13-s.jpg",
+    "intro_img":"./img/experiment/13.jpg"
   },
   {
     "id": "experiment",
     "title": "傅立葉紅外線光譜儀",
-    "label": "exp14_label",
+    "intro_title": "ASTM : E-1421",
     "target": "#exp14",
     "target_id": "exp14",
-    "img": "./img/experiment/14.jpg"
+    "img": "./img/experiment/14-s.jpg",
+    "intro_img":"./img/experiment/14.jpg"
   },
   {
     "id": "experiment",
     "title": "潤滑脂錐入度測定儀",
-    "label": "exp15_label",
+    "intro_title": "ASTM : E-1421",
     "target": "#exp15",
     "target_id": "exp15",
-    "img": "./img/experiment/15.jpg"
+    "img": "./img/experiment/15-s.jpg",
+    "intro_img":"./img/experiment/15.jpg"
   },
   {
     "id": "experiment",
     "title": "寬潤滑脂溫度範圍滴定測定儀",
-    "label": "exp16_label",
+    "intro_title": "ASTM : D217",
     "target": "#exp16",
     "target_id": "exp16",
-    "img": "./img/experiment/16.jpg"
+    "img": "./img/experiment/16-s.jpg",
+    "intro_img":"./img/experiment/16.jpg"
   },
   {
     "id": "experiment",
     "title": "XRF光譜",
-    "label": "exp17_label",
+    "intro_title": "XRF光譜儀\nASTM : D7751",
     "target": "#exp17",
     "target_id": "exp17",
-    "img": "./img/experiment/17.jpg"
+    "img": "./img/experiment/17-s.jpg",
+    "intro_img":"./img/experiment/17.jpg"
   }
 ]
 
